f05d341bff06dd416dbf49ac98988a1dead60644
larhzizelnisrine <nisrinejiji123@hotmail.com>
2021-02-06 23:07:24 +0100

    changed

--- TP1/ex01/exercice_1a.c
+++ TP1/ex01/exercice_1a.c
@@ -8,6 +8,17 @@ float (float A[N][N], float B[N], int n)
   float   *x;
 
   x = malloc(sizeof(float) * n);
+   x[0] = B[0] / A[0][0];
+
+  for (i= 1; i < n; i++)
+  {
+	  s = 0;
+          for (j = 0; j <= i-1; j++)
+	  {
+		  s += A[i][j] * x[j];
+          }
+	  x[i] = (B[i] - s) / A[i][i];
+  }
 
   return (x);
 }
